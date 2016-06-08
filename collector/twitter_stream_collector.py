f19c8a24daf5d30dc34255f50c7183de5eb16374
cim3x <filip.maks.nowakowski@gmail.com>
2016-06-08 11:15:05 +0100

    Stream collector

---
+++ collector/twitter_stream_collector.py
@@ -0,0 +1,43 @@
+from twitter_collector import Twitter_Collector
+import tweepy
+import json
+import MySQLdb
+from tweet import Tweet
+import time
+
+class Twitter_Stream_Collector(Twitter_Collector):
+
+	class _StreamListener(tweepy.StreamListener):
+
+		def __init__(self, outer):
+			self.outer = outer
+
+		def on_data(self, data):
+			_json = json.loads(data)
+			parsed = self.outer.process_data(_json)
+			self.outer.store_data()
+
+			return True
+
+		def on_error(self, status):
+			print(status)
+			return True
+
+#################################################################
+
+
+	def authorize(self):
+		auth = tweepy.OAuthHandler(self._CLIENT_KEY, self._CLIENT_SECRET)
+		auth.set_access_token(self._ACCESS_TOKEN, self._ACCESS_SECRET)
+
+		stream_reader = self._StreamListener(self)
+		stream = tweepy.Stream(auth, stream_reader)
+
+		return stream
+
+	def run(self):
+		print("running -----------")
+
+		self.client.filter(locations = [-6.421509, 53.189579, -6.017761, 53.447171])# FIXME
+
+		self.conn.close()
